8f49568f8aa19aa31cd5af70b164101fb6bce47c
mostlyunoriginal <lance.couzens@gmail.com>
2025-08-08 19:33:22 -0600

    reg dev update

--- scratch.py
+++ scratch.py
@@ -8,23 +8,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-cd17 = CensusData(2017)
+cd = CensusData()
 
-cd17.load_key(os.getenv("CENSUS_API_KEY"))
+cd.load_key(os.getenv("CENSUS_API_KEY"))
 
-potential_products = cd17.list_products(to_dicts=False, pattern="acs 5")
+potential_products = cd.list_products(
+    to_dicts=True,
+    pattern=[
+        "american community|acs",
+        "5-year",
+        "detailed",
+        "^(?!.*(alaska|aian|selected)).*$",
+    ],
+)
 
 for product in potential_products:
-    print(product)
+    print(product["title"], product["vintage"])
 
-cd17.set_product("ACS 5-Year Detailed Tables")
+products_df = pl.DataFrame(potential_products)
 
-potential_geos = cd17.list_geos(to_dicts=True)
+cd.set_product("ACS 5-Year Detailed Tables")
+
+potential_geos = cd.list_geos(to_dicts=True)
 
 for geo in potential_geos:
     print(geo)
 
-potential_vars = cd17.list_variables(
+potential_vars = cd.list_variables(
     to_dicts=True,
     pattern=["median household income", "inflation-adjusted", "total"],
 )
